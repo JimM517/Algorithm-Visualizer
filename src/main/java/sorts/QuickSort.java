b403dc2d345c12968551c859b4d14648c3038ec2
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-08-20 10:15:49 -0400

    Finished quick sort

--- src/main/java/sorts/QuickSort.java
+++ src/main/java/sorts/QuickSort.java
@@ -8,16 +8,18 @@ import application.SortingPanel;
  */
 public class QuickSort implements Sortable {
 
+
+
+
     @Override
-    public void sort(int[] arr, SortingPanel visualizer) {
+    public void sort(int[] arr,  SortingPanel visualizer) {
 
-        int start = 0;
-        int end = arr.length - 1;
+        // recursive divide and conquer algorithm
 
-
-
+        quickSort(arr, 0, arr.length - 1, visualizer);
 
 
+//        visualizer.updateDisplay();
     }
 
 
@@ -27,7 +29,45 @@ public class QuickSort implements Sortable {
     }
 
 
-    private void swapValues(int[] arr, int i, int j) {
+    private static void quickSort(int[] arr, int start, int end, SortingPanel visualizer) {
+
+        // base case
+        if (end <= start) return;
+
+        // get our pivot, once we find pivot we can start recursion
+        int pivot = partition(arr, start, end, visualizer);
+
+        // left partition
+        quickSort(arr, start, pivot - 1, visualizer);
+
+        // right partition
+        quickSort(arr, pivot + 1, end, visualizer);
+
+
+
+    }
+
+
+    // helper function
+    private static int partition(int[] arr, int start, int end, SortingPanel visualizer) {
+        int pivot = arr[end];
+        int i = start - 1;
+
+        for (int j = start; j <= end; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swapValues(arr, i, j);
+                visualizer.updateDisplay();
+            }
+        }
+        i++;
+        swapValues(arr, i, end);
+        visualizer.updateDisplay();
+        return i;
+    }
+
+
+    private static void swapValues(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
